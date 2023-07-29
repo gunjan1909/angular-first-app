c0c4cf87b3fd60cfabe5193ecbfe76924042bf34
gunjan1909 <bhanarkaranita@gmail.com>
2023-07-29 22:50:00 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
+
+// importing the decorator
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
+// decorator
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  //register new component here
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  //allows us to add other modules
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   //components passed for angular to bootstrap them
   //root component, all other components goes inside it
+  // bootstrap: which component should angular be aware of when whole application starts
+  // or which root component goes in indexhtml file
   bootstrap: [AppComponent],
 })
+
+//empty typescript class
 export class AppModule {}
